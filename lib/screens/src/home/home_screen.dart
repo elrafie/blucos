c5ae76b3f8a236c35c43a8ef070bbe68310f3cbf
amer <elrafiea@mcmaster.ca>
2020-12-10 17:05:52 -0500

    Tab bar creation + functionality (4 main screens created)

--- lib/screens/src/home/home_screen.dart
+++ lib/screens/src/home/home_screen.dart
@@ -1,4 +1,7 @@
-import 'package:blucos/utilities/src/constants.dart';
+import 'package:blucos/screens/src/home/curated/curated_screen.dart';
+import 'package:blucos/screens/src/home/featured/featured_screen.dart';
+import 'package:blucos/screens/src/home/social/social_screen.dart';
+import 'package:blucos/utilities/utilities.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -6,12 +9,47 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Home Screen',
-        style: TextStyle(
-          fontSize: 40,
-          color: kMainBlue,
+    return MaterialApp(
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          elevation: 0,
+        ),
+        tabBarTheme: TabBarTheme(
+          indicator: BoxDecoration(
+            color: Colors.transparent,
+          ),
+          unselectedLabelColor: kUnselectedButtonColor,
+          labelColor: kMainBlue,
+          unselectedLabelStyle: TextStyle(
+            fontFamily: kMainFontFamily,
+            fontWeight: FontWeight.bold,
+            fontSize: kTabFontSize,
+          ),
+          labelStyle: TextStyle(
+            fontFamily: kMainFontFamily,
+            fontWeight: FontWeight.bold,
+            fontSize: kTabFontSize,
+          ),
+        ),
+      ),
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: TabBar(
+            tabs: [
+              Tab(text: 'curated'),
+              Tab(text: 'featured'),
+              Tab(text: 'social'),
+            ],
+            labelPadding: EdgeInsets.only(top: kTopTabBarInset),
+          ),
+          body: TabBarView(
+            children: [
+              CuratedScreen(),
+              FeaturedScreen(),
+              SocialScreen(),
+            ],
+          ),
         ),
       ),
     );
--- lib/utilities/src/constants.dart
+++ lib/utilities/src/constants.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
+// Fonts
 const kMainFontFamily = 'Roboto';
+
+// Font size
+const kTabFontSize = 20.00;
 
 // Colors
 const kMainBlue = Color(0xFF1B1AD5);
@@ -9,3 +13,6 @@ const kMainGreen = Color(0xFF23908C);
 const kUnselectedButtonColor = Color(0xFF707070);
 const kTabBarColor = Color(0xFFe8e8e8);
 const kBackgroundColor = Color(0xFFFAFAFA);
+
+// EdgeInsets
+const kTopTabBarInset = 60.00;
